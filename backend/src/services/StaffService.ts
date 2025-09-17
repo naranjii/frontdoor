d01929b89559593ce348e2fc9389a53a57de4a64
M_Laranjeira <matlaranjeira97@gmail.com>
2025-09-17 10:52:48 +0000

    db: unique_usernames migration

--- backend/src/services/StaffService.ts
+++ backend/src/services/StaffService.ts
@@ -2,15 +2,15 @@ import { prisma } from "../config/db";
 import * as bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 
-export async function register(name: string, email: string, UserRole, password: string) {
+export async function register(name: string, password: string) {
   const hashed = await bcrypt.hash(password, 10);
-  return prisma.user.create({
-    data: { name, email, role, password: hashed }
+  return prisma.staff.create({
+    data: { name, password: hashed }
   });
 }
 
-export async function login(email: string, password: string) {
-  const user = await prisma.user.findUnique({ where: { email } });
+export async function login(name: string, password: string) {
+  const user = await prisma.staff.findUnique({ where: { name } });
   if (!user) throw new Error("User not found");
   const valid = await bcrypt.compare(password, user.password);
   if (!valid) throw new Error("Invalid password");
