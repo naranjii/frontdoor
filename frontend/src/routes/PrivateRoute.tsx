5e34c862b761e5655289e805d6d4e8b8c4f84fb3
M_Laranjeira <matlaranjeira97@gmail.com>
2025-10-09 02:10:56 +0000

    oops

--- frontend/src/routes/PrivateRoute.tsx
+++ frontend/src/routes/PrivateRoute.tsx
@@ -5,7 +5,6 @@ import { Navigate } from "react-router-dom";
 export default function PrivateRoute({ children }: { children: React.ReactNode }) {
   const auth = React.useContext(AuthContext);
   if (!auth) {
-    console.log("TA AQUI CARALHOOOOOOOO")
     return <Navigate to="/login" />;
   }
   return auth.isAuthenticated ? children : <Navigate to="/login" />;
