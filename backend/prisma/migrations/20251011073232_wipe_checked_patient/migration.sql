5987d81c63c3e28879bdbea08a226b53f8c993ba
M_Laranjeira <matlaranjeira97@gmail.com>
2025-10-11 09:28:13 +0000

    db: temp flex for Guest model (works)

---
+++ backend/prisma/migrations/20251011073232_wipe_checked_patient/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `checked` on the `Patient` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Patient` DROP COLUMN `checked`;
